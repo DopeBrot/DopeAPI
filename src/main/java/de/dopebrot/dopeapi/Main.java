9106555bf942ae219a6e5215cb6ab49511aa63fb
Hptlp <niclaselling8@gmail.com>
2022-09-16 15:18:25 +0200

    language manager implemented config added

--- src/main/java/de/dopebrot/dopeapi/Main.java
+++ src/main/java/de/dopebrot/dopeapi/Main.java
@@ -1,21 +1,36 @@
 package de.dopebrot.dopeapi;
 
+import de.dopebrot.dopeapi.command.Apfel;
 import de.dopebrot.dopeapi.command.HelpCommand;
 import de.dopebrot.dopeapi.command.TestCommand;
 import de.dopebrot.dopeapi.helper.DPCommand;
 import de.dopebrot.dopeapi.helper.LanguageHelper;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.reflect.FieldUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.FileUtil;
+import org.checkerframework.checker.units.qual.C;
 
+import java.io.*;
+import java.net.URL;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Objects;
 import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
 
 	private HelpCommand helpCommand;
 	private TestCommand testCommand;
-	private LanguageHelper languageHelper;
-
-	public final String permissionBase = "dp.itemapi";
+	private LanguageHelper language;
+	private boolean debug;
+	private String prefix = "§a[DopeAPI]";
+	public final String permissionBase = "dpapi.";
 	public ArrayList<DPCommand> commands;
 
 	public void log(String s) {
@@ -23,19 +38,95 @@ public class Main extends JavaPlugin {
 	}
 
 	public void onEnable() {
+		this.debug = false;
 		registerCommands();
-		languageHelper = new LanguageHelper("deutsch", "plugins/DPAPI/lang", "de");
-		languageHelper.setText("main.load","test");
-		languageHelper.save();
-		log(languageHelper.getString("main.load"));
-		languageHelper.debugLoad(true);
-		languageHelper.load();
-		log(languageHelper.getString("main.load"));
-		languageHelper.setText("test.kaese","dies ist eine test nachricht für kommentare",new String[]{"Comment 1","Comment 2"});
-		languageHelper.save();
+		loadDefaultConfigs();
+		loadConfig();
+		loadLanguage();
 	}
 
+	private void getResourceFile(String fileName, File file) {
+		InputStream inputStream = this.getResource(fileName);
+		try {
+			assert inputStream != null;
+			FileUtils.copyInputStreamToFile(inputStream,file);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * copy's default configs and adds it to folders if they not exist
+	 */
+	private void loadDefaultConfigs() {
+		if (new File("plugins/DopeAPI/").mkdirs()) {
+			if (debug) {
+				Bukkit.getLogger().log(Level.CONFIG, "dirs where created!");
+			}
+		}
+		if (!new File("plugins/DopeAPI/config.yml").exists()) {
+			if (debug)
+				Bukkit.getLogger().log(Level.CONFIG, "loading config from jar : config");
+			getResourceFile("config.yml", new File("plugins/DopeAPI/config.yml"));
+		}
+		if (!new File("plugins/DopeAPI/language.yml").exists()) {
+			if (debug)
+				Bukkit.getLogger().log(Level.CONFIG, "loading config from jar : config");
+			getResourceFile("language.yml", new File("plugins/DopeAPI/language.yml"));
+		}
+	}
+
+	/**
+	 * load default configs
+	 */
+	private void loadConfig() {
+		File f = new File("plugins/DopeAPI/config.yml");
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(f);
+		if (config.contains("debug")) {
+			this.debug = config.getBoolean("debug", false);
+		}
+	}
+
+	private void loadLanguage() {
+		language = new LanguageHelper("language", "plugins/DopeAPI", "lang");
+		language.load();
+		Bukkit.getLogger().log(Level.INFO, getString("main.language"));
+		prefix = getString("prefix");
+	}
+	/**
+	 * @param key key of text e.g. (command.help.lack.permission)
+	 * @return text or null of the key cant be found!
+	 */
+	public String getString(String key) {
+		return language.getString(key);
+	}
+
+	/**
+	 * registers commands
+	 */
 	private void registerCommands() {
+		commands = new ArrayList<>();
+		commands.add(new HelpCommand(this));
+		commands.add(new TestCommand(this));
+
+		for (DPCommand c : commands) {
+			if (c == null) {
+				continue;
+			}
+
+			this.getCommand(c.getCommandName());
+			this.getCommand(c.getCommandName()).setExecutor(c);
+
+			if (debug) {
+				log("register command [" + c.getCommandName() + "]");
+				for (String p : c.getPermissions()) {
+					log("  perm > " + p.replaceAll("..", "."));
+				}
+
+			}
+
+		}
+
 	}
 
 	public void onDisable() {
