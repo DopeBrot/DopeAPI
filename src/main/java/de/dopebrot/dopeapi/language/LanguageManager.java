367f4b4e3816bb3be99aad7c280e4590757e04fa
Hptlo <niclaselling8@gmail.com>
2022-12-11 01:25:37 +0100

    added Information. removed double load method

--- src/main/java/de/dopebrot/dopeapi/language/LanguageManager.java
+++ src/main/java/de/dopebrot/dopeapi/language/LanguageManager.java
@@ -2,6 +2,7 @@ package de.dopebrot.dopeapi.language;
 
 import de.dopebrot.dopeapi.config.DPConfig;
 import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,6 +17,11 @@ public class LanguageManager {
 	private boolean debug = false;
 	private String errorMessage;
 
+	/**
+	 * creates a new Language Manager
+	 * @param folder where are all the language configs saved
+	 * @apiNote all files in this directory need to be an "*.json"
+	 */
 	public LanguageManager(File folder) {
 		this.languageFolder = folder;
 		this.errorMessage = "%key%:%message% was not found report this to an administrator";
@@ -29,19 +35,36 @@ public class LanguageManager {
 		}
 	}
 
+	/**
+	 * if debug messages should be shown
+	 *
+	 * @param debug true or false
+	 */
 	public void setDebug(boolean debug) {
 		this.debug = debug;
 	}
 
+	/**
+	 * logs messages in console
+	 *
+	 * @param s String that is written
+	 */
 	private void log(String s) {
 		Bukkit.getLogger().log(Level.WARNING, s);
 	}
 
+	/**
+	 * sets the error message if the message is not found
+	 * @param errorMessage string of the new message
+	 */
 	public void setErrorMessage(String errorMessage) {
 		this.errorMessage = errorMessage;
 	}
 
-	private void loadFiles() {
+	/**
+	 * @apiNote gets all files from language folder and puts them in memory.
+	 */
+	public void load() {
 		if (languageFolder.isDirectory()) {
 			if (languageFolder.listFiles() != null) {
 				for (File currentFile : languageFolder.listFiles()) {
@@ -72,16 +95,17 @@ public class LanguageManager {
 		}
 	}
 
-	public void load() {
-		loadFiles();
-	}
-
+	/**
+	 * @param key     key of the language (key could be "en" for English)
+	 * @param message the message that is searched (message could be "player_disconnect")
+	 * @return either an error string that the message couldn't be found or the actual message.
+	 */
 	public String getString(String key, String message) {
-			if (languageIdentifier.containsKey(key)) {
-				if (languageIdentifier.get(key).has(message)) {
-					return languageIdentifier.get(key).message(message);
-				}
+		if (languageIdentifier.containsKey(key)) {
+			if (languageIdentifier.get(key).has(message)) {
+				return languageIdentifier.get(key).message(message);
 			}
+		}
 		return errorMessage.replace("%key%", key).replace("%message%", message);
 	}
 
