df7f4e7897dd5ce60770f1d32c60e8fe526324bf
Hptlo <niclaselling8@gmail.com>
2022-12-13 17:29:38 +0100

    java doc

--- src/main/java/de/dopebrot/dopeapi/helper/DPCommand.java
+++ src/main/java/de/dopebrot/dopeapi/helper/DPCommand.java
@@ -2,4 +2,9 @@ package de.dopebrot.dopeapi.helper;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
 
+/**
+ * this class adds CommandExecutor and TabCompleter in one implementation
+ * @since 1.0
+ * @author DopeBrot
+ */
 public interface DPCommand extends CommandExecutor, TabCompleter {}
