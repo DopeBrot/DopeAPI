669d0d83eaa46d3fdf18594f7adfd374eb9c4fa5
Hptlo <niclaselling8@gmail.com>
2022-12-23 16:53:00 +0100

    added java doc & added method getRegionsInside

--- src/main/java/de/dopebrot/dopeapi/region/RegionManager.java
+++ src/main/java/de/dopebrot/dopeapi/region/RegionManager.java
@@ -5,6 +5,15 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * This RegionManager can add/remove regions from a list.<br></br>
+ * you can check if a location is inside a region and can return all regions.
+ *
+ * @author DopeBrot
+ * @see Region
+ * @since 7.0
+ */
+
 public class RegionManager {
 
 	private final ArrayList<Region> regions;
@@ -15,7 +24,10 @@ public class RegionManager {
 
 	/**
 	 * adds a region
+	 *
 	 * @param region the region that needs to be added
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	public void addRegion(Region region) {
 		this.regions.add(region);
@@ -25,6 +37,8 @@ public class RegionManager {
 	 * removes a given region
 	 *
 	 * @param region the region that needs to be removed
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	public void removeRegion(Region region) {
 		this.regions.remove(region);
@@ -34,6 +48,8 @@ public class RegionManager {
 	 * removes a region accordingly to their uuid
 	 *
 	 * @param uuid the uuid of the region that needs to be removed
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	public void removeRegion(UUID uuid) {
 		for (Region currentRegion : regions) {
@@ -48,6 +64,8 @@ public class RegionManager {
 	 * removes a region accordingly to their name
 	 *
 	 * @param regionName the name of the region that needs to be removed
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	public void removeRegion(String regionName) {
 		for (Region currentRegion : regions) {
@@ -61,6 +79,8 @@ public class RegionManager {
 	/**
 	 * @param location location that needs to be checked
 	 * @return true if location is inside any region
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	public boolean isInside(Location location) {
 		for (Region currentRegion : regions) {
@@ -78,8 +98,32 @@ public class RegionManager {
 	}
 
 	/**
+	 * @param location location that needs to be searched for
+	 * @return {@link Region} array that are inside the location
+	 * @author DopeBrot
+	 * @since 7.0
+	 */
+	public Region[] getInside(Location location) {
+		ArrayList<Region> regionList = new ArrayList<>();
+		for (Region currentRegion : regions) {
+			if (currentRegion.getStart().getX() > location.getX()
+					&& currentRegion.getStart().getY() > location.getY()
+					&& currentRegion.getStart().getZ() > location.getZ()) {
+				if (currentRegion.getEnd().getX() < location.getX()
+						&& currentRegion.getEnd().getY() < location.getY()
+						&& currentRegion.getEnd().getZ() < location.getZ()) {
+					regionList.add(currentRegion);
+				}
+			}
+		}
+		return regionList.toArray(new Region[0]);
+	}
+
+	/**
 	 * @param location location of the region that need to be found
 	 * @return region where the location is inside
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	@Nullable
 	public Region getRegion(Location location) {
@@ -99,6 +143,9 @@ public class RegionManager {
 
 	/**
 	 * return ArrayList of all region there are
+	 *
+	 * @author DopeBrot
+	 * @since 7.0
 	 */
 	public ArrayList<Region> getRegions() {
 		return regions;
